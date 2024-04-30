abb8cf568dadbffaf96557731bb13999a21ee1eb
aleixmascaro <149720702+amascaju@users.noreply.github.com>
2024-04-30 19:42:12 +0200

    puntuacio

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private List<TextMeshProUGUI> puntuationTextList;
 
+    private Dictionary<DianaController.PointsHit, TextMeshProUGUI> canvasPuntuation;
     private Dictionary<DianaController.PointsHit, int> puntuationList;
+
+    private int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         puntuationList = new Dictionary<DianaController.PointsHit, int>();
+        canvasPuntuation = new Dictionary<DianaController.PointsHit, TextMeshProUGUI>();
+        foreach (DianaController.PointsHit typePoint in Enum.GetValues(typeof(DianaController.PointsHit)))
+        {
+            puntuationList.Add(typePoint, 0);
+            canvasPuntuation.Add(typePoint, puntuationTextList[count]);
+            Debug.Log(typePoint + " " + canvasPuntuation[typePoint].gameObject.ToString());
+            count++;
+        }
     }
 
     // Update is called once per frame
@@ -19,9 +34,10 @@ public class GameManager : MonoBehaviour
         
     }
 
-    // Pensar com emplear enum perque sigui un array
     public void SetNewPuntuation(DianaController.PointsHit pointType)
     {
         puntuationList[pointType]++;
+        canvasPuntuation[pointType].text = puntuationList[pointType].ToString();
+        Debug.Log(pointType + ": " + puntuationList[pointType]);
     }
 }
